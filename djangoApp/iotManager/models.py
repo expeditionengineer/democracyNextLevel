1e508f24dffeadb36184128276477c800254bc12
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-05-11 16:38:32 +0200

    working on openstreetmap integration to show the location of the iotDevices

--- djangoApp/iotManager/models.py
+++ djangoApp/iotManager/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-from django.contrib.gis.db import models
 
 
 class IotDevice(models.Model):
-    geoCoordinates = models.PointField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    typeOfDevice = models.CharField(max_length=100)
+    deviceName = models.CharField(max_length=100)
+    deviceURI = models.CharField(max_length=100)
+    deviceDescription = models.TextField()
+    deviceStatus = models.BooleanField(default=False)
+    deviceOwner = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    deviceImage = models.ImageField(upload_to="images/", blank=True, null=True)
