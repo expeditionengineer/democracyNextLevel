5e181e0c22e424acb8c28bd783eabdda5bea2da7
Samuel <s.merkel@tu-berlin.de>
2024-07-15 20:55:57 +0200

    safed example slides in data/exampleSlidesData

--- frontend/src/pages/DisplaySlideshow.js
+++ frontend/src/pages/DisplaySlideshow.js
@@ -1,47 +1,6 @@
 import SlideTemplate from '../components/SlideTemplate';
 import Carousel from 'react-bootstrap/Carousel';
-
-const exempleSlides = [
-  {
-    "id": 0,
-    "title": "Clean Up",
-    "description": "Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber.",
-    "author": "Can",
-    "startDate": "2024-07-15 12:00",
-    "endDate": "2024-07-15 14:00",
-    "repeated": "jede Woche",
-    "createdAt": "2024-07-14",
-    "organizer": "Dorfwerkstatt",
-    "published": "2024-07-14",
-    "updatedAt": ""
-  },
-  {
-    "id": 1,
-    "title": "Kindermalen",
-    "description": "Buntes Malen mit Wasserfarben und Stiften",
-    "author": "Minnie Mouse",
-    "startDate": "2024-10-10 10:00",
-    "endDate": "2024-10-10 14:30",
-    "repeated": false,
-    "createdAt": "2024-07-12",
-    "organizer": "Gottfriedkeller Gymnasium",
-    "published": "2024-07-12",
-    "updatedAt": ""
-  },
-  {
-    "id": 2,
-    "title": "Clean Up",
-    "description": "Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber. Wir machen die Mierendorff-Straße sauber.",
-    "author": "Can",
-    "startDate": "2024-07-15 12:00",
-    "endDate": "2024-07-15 14:00",
-    "repeated": "jede Woche",
-    "createdAt": "2024-07-14",
-    "organizer": "Dorfwerkstatt",
-    "published": "2024-07-14",
-    "updatedAt": ""
-  },
-];
+import exampleSlides from '../data/exampleSlidesData';
 
 function calculateSlideInterval(slide) {
   const textLength = Object.values(slide).reduce((len, txt) =>
@@ -51,7 +10,7 @@ function calculateSlideInterval(slide) {
 }
 
 function DisplaySlideshow() {
-  const slides = exempleSlides.map(slide => (
+  const slides = exampleSlides.map(slide => (
     <Carousel.Item className="carousel-item" interval={calculateSlideInterval(slide)}>
       <SlideTemplate
         slide={slide}
